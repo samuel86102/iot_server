272385f34675e93574c1156310175123d3a99226
samuel <samuel86102@gmail.com>
2022-01-02 20:04:45 +0800

    add new

--- ui_template/app.py
+++ ui_template/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index_ver2.html')
 
 if __name__ == '__main__':
     app.debug = True
